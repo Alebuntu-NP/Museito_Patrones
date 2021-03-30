0ec36c5ac6028674f0a28756924e1b54be72bc6e
empichi <74982353+empichi@users.noreply.github.com>
2021-03-30 13:46:59 +0200

    Creando IObra.java
    He añadido la interfaz de obra

---
+++ Museito/src/museito/IObra.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package museito;
+
+/**
+ *
+ * @author empic
+ */
+public interface IObra {
+
+    String getId();
+
+    String getNombre();
+
+    String getTipo();
+
+    int getAnyo();
+
+    String getOrigen();
+    
+    Calendar getFecha_reparacion();
+    
+    String getAutor();
+    
+    boolean getestado();
+    
+    String getDescripcion();
+
+    void setId(String id);
+    
+    void setNombre(String nombre);
+    
+    void setTipo(String tipo);
+    
+    void setEstilo(String estilo);
+    
+    void setAnyo(int anyo);
+    
+    void setOrigen(String origen);
+    
+    void setFecha_reparacion(Calendar fecha_reparacion);
+    
+    void setAutor(String autor);
+    
+    void setEstado(boolean estado);
+    
+    void setDescripcion(String descripcion);
+
+}
