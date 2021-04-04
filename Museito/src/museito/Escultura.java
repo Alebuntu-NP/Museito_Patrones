d9a2f1b99cb45a12da0649874ac74c8af7d637a1
algargon99 <33413236+algargon99@users.noreply.github.com>
2021-04-04 14:27:07 +0200

    Correccion Escultura

--- Museito/src/museito/Escultura.java
+++ Museito/src/museito/Escultura.java
@@ -7,7 +7,7 @@ public class Escultura extends Obra{
     
     private String material;
 
-    public Escultura(String id, String nombre, String tipo, String estilo, int anyo, String origen, Calendar fecha_reparacion, String autor, String estado, boolean reparado, String descripcion, String materials) {
+    public Escultura(String id, String nombre, String tipo, String estilo, int anyo, String origen, Calendar fecha_reparacion, String autor, String estado, boolean reparado, String descripcion, String material) {
         super(id, nombre, tipo, estilo, anyo, origen, fecha_reparacion, autor, estado, reparado, descripcion);
         this.material = material;
     }
