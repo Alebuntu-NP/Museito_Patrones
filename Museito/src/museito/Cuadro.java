d67d162477e62a61e9becaf955dbc659fdc85841
empichi <74982353+empichi@users.noreply.github.com>
2021-04-04 14:26:03 +0200

    Cuadro.java

---
+++ Museito/src/museito/Cuadro.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package museito;
+
+import java.util.Calendar;
+
+/**
+ *
+ * @author empic
+ */
+public class Cuadro extends Obra{
+    private String marco;
+    
+     public Cuadro(String id, String nombre, String tipo, String estilo, int anyo, String origen, Calendar fecha_reparacion, String autor, String estado, boolean reparado, String descripcion, String marco){
+        super(id, nombre, tipo, estilo, anyo, origen, fecha_reparacion, autor, estado, reparado, descripcion);
+        this.marco = marco;
+        
+    }
+
+    public String getMarco() {
+        return marco;
+    }
+
+    public void setMarco(String marco) {
+        this.marco = marco;
+    }
+     
+}
